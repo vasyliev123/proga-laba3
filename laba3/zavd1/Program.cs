76f245dd6201aa57a748011325ac3fbb46dda7b0
averageJavaEnjoyer <75797989+vasyliev123@users.noreply.github.com>
2022-05-17 10:16:50 +0300

    Add files via upload

---
+++ laba3/zavd1/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace zavd1
+{
+    class Program
+    {
+        //Створити об'єкт класу Дерево, використовуючи класи Лист, Гілка. Методи: зацвісти, опасти листю, покритися інеєм, пожовтіти листю.
+        static void Main(string[] args)
+        {
+            Leaf l1 = new ("Lystok 1");
+            Leaf l2 = new ("Lystok 2");
+            Leaf l3 = new ("Lystok 3");
+            Leaf l4 = new ("Lystok 4");
+
+            Branch<Leaf> b1 = new ("Vitka 1");
+            Branch<Leaf> b2 = new ("Vitka 2");
+
+            Tree<Branch<Leaf>> t1 = new ("Derevo 1");
+
+            b1.getHashSet().Add(l1);
+            b1.getHashSet().Add(l2);
+            b1.getHashSet().Add(l3);
+            b2.getHashSet().Add(l4);
+                            
+            t1.getHashSet().Add(b1);
+            t1.getHashSet().Add(b2);
+
+            t1.PokrytysIniem();
+            t1.Zacvisty();
+
+            foreach(Branch<Leaf> b in t1.getHashSet())
+            {
+                foreach(Leaf l in b.getHashSet())
+                {
+                    l.Opasty();
+                }
+            }
+
+            foreach (Branch<Leaf> b in t1.getHashSet())
+            {
+                foreach (Leaf l in b.getHashSet())
+                {
+                    l.Pozhovtity();
+                }
+            }
+           // Console.WriteLine(l1.GetHashCode());
+        }
+    }
+    public class Leaf
+    {
+        private String text;
+
+        public Leaf(String text)
+        {
+            this.text = text;
+        }
+
+        public void Opasty()
+        {
+            Console.WriteLine($"{text} opav");
+        }
+        public void Pozhovtity()
+        {
+            Console.WriteLine($"{text} pozhovtiv");
+        }
+
+        public bool Equals(Leaf obj)
+        {
+            if(obj==null)
+            {
+                return false;
+            }
+            return obj.text == text;
+        }
+        public override int GetHashCode()
+        {
+            return text.GetHashCode();
+        }
+
+
+        public override string ToString()
+        {
+            return text;
+        }
+    }
+    public class Branch<T>
+    {
+        
+        private string text;
+        private HashSet<T> list;
+
+        public Branch(string text)
+        {
+            this.text = text;
+            list = new HashSet<T>();
+        }
+
+        
+
+        public HashSet<T> getHashSet()
+        {
+            return list;
+        }
+
+        public bool Equals(Branch<T> obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            return obj.text == text;
+        }
+        public override int GetHashCode()
+        {
+            return text.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return text;
+        }
+    }
+    public class Tree<T>
+    {
+        private string text;
+        private HashSet<T> list;
+
+        public Tree(string text)
+        {
+            this.text = text;
+            list = new HashSet<T>();
+        }
+
+        public void Zacvisty()
+        {
+            Console.WriteLine("Derevo cvite");
+        }
+        public void PokrytysIniem()
+        {
+            Console.WriteLine("Derevo pokrulos ininem");
+        }
+
+        public bool Equals(Tree<T> obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            return obj.text == text;
+        }
+        public override int GetHashCode()
+        {
+            return text.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return text;
+        }
+
+        public HashSet<T> getHashSet()
+        {
+            return list;
+        }
+
+        
+    }
+}
---
+++ laba3/zavd2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+namespace zavd2
+{
+    class Program
+    {
+        //Таксопарк. Визначити ієрархію легкових автомобілів. Створити таксопарк.
+        //Підрахувати вартість автопарку. Провести сортування автомобілів парку по
+        //витраті палива. Знайти автомобіль в компанії, що відповідає заданому діапазону параметрів швидкості.
+
+        static void Main(string[] args)
+        {
+            Avto avto1 = new("Mazda", 9999, 60, 100);
+
+            Avto avto2 = new("Honda", 2399, 450, 120);
+
+            Avto avto3 = new("Bmb", 15999, 8, 200);
+
+            Avto avto4 = new("Tesla", 24999, 0, 300);
+
+            TaksoPark tp = new TaksoPark();
+
+            tp.list.Add(avto1);
+            tp.list.Add(avto2);
+            tp.list.Add(avto3);
+            tp.list.Add(avto4);
+
+            tp.ShowList();
+
+            tp.Vartist();
+
+            tp.sortVytraty();
+
+            tp.ShowList();
+
+            tp.shv_search(100, 120);
+        }
+    }
+
+    class TaksoPark
+    {
+        public List<Avto> list = new List<Avto>();
+        
+
+        public void Vartist()
+        {
+
+            double res = 0;
+            foreach(Avto a in list)
+            {
+                res += a.Price;
+            }
+            Console.Write($"\nZagalna vartist takso parku: {res}\n");
+           
+        }
+
+        public void sortVytraty()
+        { 
+            IEnumerable<Avto> q = list.OrderBy(a => a.VytratyPalyva);
+            list = q.ToList();
+            Console.WriteLine("\nSorted po vytraty palnogo\n");
+        }
+        public void ShowList()
+        {
+           
+            foreach (Avto a in list)
+            {
+                Console.WriteLine($"{a.Name}\t{a.Price}\t{a.VytratyPalyva}\t{a.MaxShv}");
+            }
+        }
+        public void shv_search(double c, double b)
+        {
+            Console.WriteLine("\nPoshuk po shvydkosti\n");
+            List<Avto> newList = new List<Avto>();
+
+            foreach(Avto a in list.Where(x=> x.MaxShv>=c&&x.MaxShv<=b))
+            {
+                Console.WriteLine($"{a.Name}\t{a.Price}\t{a.VytratyPalyva}\t{a.MaxShv}");
+            }
+        }
+
+    }
+
+    class Avto
+    {
+
+        
+        public Avto(string n, double p, double vp, double ms)
+        {
+            Name = n;
+            Price = p;
+            VytratyPalyva = vp;
+            MaxShv = ms;
+        }
+       
+        public string Name
+        {
+            get;
+            set;
+        }
+        public double Price
+        {
+            get;
+            set;
+        }
+        public double VytratyPalyva
+        {
+            get;
+            set;
+        }
+        public double MaxShv
+        {
+            get;
+            set;
+        }
+    }
+}
